774133c2f34cef2a56e64fa68f25e7e0c9de3a2a
Filip-FK-Kwiatkowski <fk@pjwstk.edu.pl>
2024-10-08 11:34:02 +0200

    Add untested `vowelCount()` and `isVowel()` methods

--- src/BasicUtils.java
+++ src/BasicUtils.java
@@ -1,3 +1,18 @@
 public class BasicUtils {
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
 
+    public static int vowelCount(String str) {
+        long count = str.toLowerCase().chars()
+                .mapToObj(i -> (char) i)
+                .filter(BasicUtils::isVowel)
+                .count();
+
+        return (int) count;
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeio".indexOf(c) != -1;
+    }
 }
